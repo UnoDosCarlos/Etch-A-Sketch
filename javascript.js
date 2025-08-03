4460ac7c178f9a4c584333c80a742ee13f44657f
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-08-03 16:36:04 -0400

    Add 16x16 Grid
    Added a 16x16 grid to create base structure how Etch-A-Sketch program.

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const gridContainer = document.querySelector('#gridContainer');
+
+function createGrid(numDivs) {
+    for(let i = 0; i < numDivs; i++) {
+        let panel = document.createElement('div');
+        panel.classList.add('panel');
+        gridContainer.appendChild(panel);
+
+    }
+};
+
+createGrid(16);
