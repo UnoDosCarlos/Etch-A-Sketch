13af195309c4b8e3a639704ff60d7cb3fa3b7883
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-08-05 12:19:24 -0400

    Add Grid adjuster button and functionality
    Added a grid adjuster to allow user to set own grid between 1-100.

--- javascript.js
+++ javascript.js
@@ -1,41 +1,46 @@
 const gridContainer = document.querySelector('#gridContainer');
-let numDivs;
+const panels = document.querySelectorAll('.panel');
+const gridAdjusterbtn = document.querySelector('#gridAdjusterbtn');
+
+
+
 function createGrid(gridContainer, numDivs) {
-    for(let i = 0; i < numDivs * numDivs; i++) {
+    for(i = 0; i < numDivs * numDivs; i++) {
         let panel = document.createElement('div');
         panel.classList.add('panel');
+
         gridContainer.appendChild(panel);
 
-    }
-};
+        panel.addEventListener('mouseover', () => {
+            panel.style.backgroundColor = 'lightblue';
+    })
+}};
 
 createGrid(gridContainer, 16);
 
-const panels = document.querySelectorAll('.panel');
-
-
-for(const panel of panels) {
-
-
-panel.addEventListener('mouseover', () => {
-        panel.style.backgroundColor = 'lightblue';
-
-});
-};
-
-const gridAdjusterbtn = document.querySelector('#gridAdjusterbtn');
 
 gridAdjusterbtn.addEventListener('click', () => {
-        let userInput = prompt("Enter a new grid size:");
-        let newGrid = userInput*userInput;
-        
-        for(const panel of panels) {
+        let newGridNumber = parseInt(prompt("Enter a new grid size:"));
+        if(newGridNumber !== null && newGridNumber <= 100 && newGridNumber > 0) {
 
-            gridContainer.removeChild(panel);
+            gridContainer.replaceChildren();
+
+
+                for(i = 0; i < newGridNumber*newGridNumber; i++) {
+
+                        let panel = document.createElement('div');
+                        panel.classList.add('panel');
+                        panel.style.flex = `0 0 calc(100% / ${newGridNumber})`;
+
+                        gridContainer.appendChild(panel);
+
+                        panel.addEventListener('mouseover', () => {
+                            panel.style.backgroundColor = 'lightblue';
+
+                        });
+                    }
+            } else {
+            alert('Please enter a number between 1-100');
         }
-        
-        createGrid(newGrid);
-        console.log(newGrid);
+    });
 
-
-});
