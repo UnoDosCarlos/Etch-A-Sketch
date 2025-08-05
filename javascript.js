2842d28763b73aab3d31b664ab5c4e6b23b3b05d
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-08-05 15:23:27 -0400

    Add Reset Button for Etch-A-Sketch
    Added a reset button to allow user to draw without resizing grid.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,25 @@
 const gridContainer = document.querySelector('#gridContainer');
-const panels = document.querySelectorAll('.panel');
-const gridAdjusterbtn = document.querySelector('#gridAdjusterbtn');
-
+const panels = document.querySelectorAll('.panels');
+const gridAdjusterbtn = document.querySelector('#gridAdjusterBtn');
+const clearBtn = document.querySelector('#clearBtn');
 
 
 function createGrid(gridContainer, numDivs) {
     for(i = 0; i < numDivs * numDivs; i++) {
-        let panel = document.createElement('div');
-        panel.classList.add('panel');
+        let panels = document.createElement('div');
+        panels.classList.add('panels');
 
-        gridContainer.appendChild(panel);
+        gridContainer.appendChild(panels);
 
-        panel.addEventListener('mouseover', () => {
-            panel.style.backgroundColor = 'lightblue';
+        panels.addEventListener('mouseover', () => {
+            panels.style.backgroundColor = 'lightblue';
     })
 }};
 
 createGrid(gridContainer, 16);
 
 
-gridAdjusterbtn.addEventListener('click', () => {
+gridAdjusterBtn.addEventListener('click', () => {
         let newGridNumber = parseInt(prompt("Enter a new grid size:"));
         if(newGridNumber !== null && newGridNumber <= 100 && newGridNumber > 0) {
 
@@ -28,14 +28,14 @@ gridAdjusterbtn.addEventListener('click', () => {
 
                 for(i = 0; i < newGridNumber*newGridNumber; i++) {
 
-                        let panel = document.createElement('div');
-                        panel.classList.add('panel');
-                        panel.style.flex = `0 0 calc(100% / ${newGridNumber})`;
+                        let panels = document.createElement('div');
+                        panels.classList.add('panels');
+                        panels.style.flex = `0 0 calc(100% / ${newGridNumber})`;
 
-                        gridContainer.appendChild(panel);
+                        gridContainer.appendChild(panels);
 
-                        panel.addEventListener('mouseover', () => {
-                            panel.style.backgroundColor = 'lightblue';
+                        panels.addEventListener('mouseover', () => {
+                            panels.style.backgroundColor = 'lightblue';
 
                         });
                     }
@@ -44,3 +44,9 @@ gridAdjusterbtn.addEventListener('click', () => {
         }
     });
 
+clearBtn.addEventListener('click', () => {
+    const panels = document.querySelectorAll('.panels');
+        panels.forEach(panel => {
+            panel.style.backgroundColor =  'lightgray';            
+        })
+    });
