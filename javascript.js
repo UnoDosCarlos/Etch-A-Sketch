c935cf8aa87aa0d608797611f684fedc21f76d1b
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-08-10 18:56:36 -0400

    Add color changing panels with increasing opacity
    Added panels that increase in opacity by 10% every time the panel is hovered.

--- javascript.js
+++ javascript.js
@@ -3,6 +3,16 @@ const panels = document.querySelectorAll('.panels');
 const gridAdjusterbtn = document.querySelector('#gridAdjusterBtn');
 const clearBtn = document.querySelector('#clearBtn');
 
+function getRandomRgbColor() {
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+
+}
+
 
 function createGrid(gridContainer, numDivs) {
     for(i = 0; i < numDivs * numDivs; i++) {
@@ -11,9 +21,19 @@ function createGrid(gridContainer, numDivs) {
 
         gridContainer.appendChild(panels);
 
+        panels.style.opacity = 0;
+
+
         panels.addEventListener('mouseover', () => {
-            panels.style.backgroundColor = 'lightblue';
-    })
+            panels.style.backgroundColor = getRandomRgbColor();
+            let currentOpacity = parseFloat(panels.style.opacity);
+            const incrementValue = .1;
+            currentOpacity += incrementValue;
+            if(currentOpacity > 1) {
+                currentOpacity = 1;
+            }
+            panels.style.opacity = currentOpacity;
+        })
 }};
 
 createGrid(gridContainer, 16);
@@ -34,9 +54,17 @@ gridAdjusterBtn.addEventListener('click', () => {
 
                         gridContainer.appendChild(panels);
 
-                        panels.addEventListener('mouseover', () => {
-                            panels.style.backgroundColor = 'lightblue';
+                        panels.style.opacity = 0;
 
+                        panels.addEventListener('mouseover', () => {
+                            panels.style.backgroundColor = getRandomRgbColor();
+                            let currentOpacity = parseFloat(panels.style.opacity);
+                            const incrementValue = .1;
+                            currentOpacity += incrementValue;
+                            if(currentOpacity > 1) {
+                                currentOpacity = 1;
+                            }
+                            panels.style.opacity = currentOpacity;
                         });
                     }
             } else {
@@ -47,6 +75,9 @@ gridAdjusterBtn.addEventListener('click', () => {
 clearBtn.addEventListener('click', () => {
     const panels = document.querySelectorAll('.panels');
         panels.forEach(panel => {
-            panel.style.backgroundColor =  'lightgray';            
+            panel.style.backgroundColor =  'lightgray';
+            panel.style.opacity = 0;          
         })
     });
+
+
